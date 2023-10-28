2f6e6d8c3b947657a6cad26a37f2fb46ce3826ba
akshatapurva213 <149247209+akshatapurva213@users.noreply.github.com>
2023-10-28 20:01:36 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 21, 43, 22, 67, 43, 12, 4 };
+            int i =arr[0], j = arr[5];
+            for (i = 0; i < arr.Length ; j < arr.Length ; i++;j--) {
+                Console.ReadLine(arr);
+            }
+
+
+        }
+    }
+}
